76d69c1b3c6c9ba0196771ee3cc4ac9c2b1d876a
Rishi Rawat <121020112+rishirawat04@users.noreply.github.com>
2024-07-29 15:09:58 +0530

    commit for dataTabs

--- src/domains/Dashboard/components/TabData.tsx
+++ src/domains/Dashboard/components/TabData.tsx
@@ -6,12 +6,15 @@ const TabData: React.FC<TabProps> = ({ title, number, growth }) => {
   // Format the number with commas
   const formattedNumber = number.toLocaleString();
 
+  // Determine the color class based on growth percentage
+  const growthClass = growth > 0 ? (growth < 25 ? 'text-red-500' : 'text-green-500') : 'text-red-500';
+
   return (
-    <div className="p-2 w-72  bg-white rounded-lg border border-gray-200  m-4" style={{ borderRadius: '10px' }}>
-      <h2 className="text-gray-700 mb-2">{title}</h2>
+    <div className="p-3 w-72 bg-white rounded-lg border border-gray-200 m-4" style={{ borderRadius: '10px' }}>
+      <h3 className="text-gray-500 text-sm mb-2">{title}</h3>
       <div className="flex items-center justify-between">
-        <span className="text-3xl font-bold">{formattedNumber}</span>
-        <div className={`flex items-center ${growth > 0 ? 'text-green-500' : 'text-red-500'}`}>
+        <span className="text-2xl font-bold">{formattedNumber}</span>
+        <div className={`flex items-center ${growthClass}`}>
           <span className="ml-1">{growth}%</span>
           {growth > 0 ? <FaArrowUp /> : <FaArrowDown />}
         </div>
--- src/domains/Dashboard/pages/Dashboard.tsx
+++ src/domains/Dashboard/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ const Dashboard: React.FC = () => {
   const data = [
     { title: 'TOTAL ACTIVE ANTOILERS', number: 120, growth: 15 },
     { title: 'TOTAL ACTIVE USERS', number: 75, growth: -5 },
-    { title: 'TOTAL CONTACTED ', number: 20000, growth: 10 },
+    { title: 'TOTAL CONTACTED ', number: 20000, growth: 30 },
     { title: 'TOTAL HIRED ', number: 20000, growth: 10 },
   ];
 
